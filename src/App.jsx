846f1e8a51834652df0697d375dff8cc6ab72332
CCode02 <cristiancameloariza@gmail.com>
2026-04-20 15:03:30 -0500

    Implementación de filtrado de paises con el input

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,33 @@ import CountryCard from "./components/CountryCard"
 function App() {
 
   const [countries, setCountries] = useState([])
+  const [countriesFiltered, setCountriesFiltered] = useState([])
+  const [countryName, setCountryName] = useState('')
 
   useEffect(() => {
-    countriesService.getAll().then(response => setCountries(response))
+    countriesService.getAll().then(response => {
+      setCountries(response)
+      setCountriesFiltered(response)
+    })
   }, [])
+
+  const handleCountryNameChange = (event) => {
+    setCountryName(event.target.value)
+    setCountriesFiltered(countries.filter(c => c.name.common.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
 
   return (
     <div className="flex flex-col w-full h-screen p-12 items-center box-border">
       <h1 className="text-6xl font-bold">
         Search Country
       </h1>
-      <input type="text" className="border py-2 px-4 text-4xl rounded-4xl mt-8" />
+      <input 
+      type="text" 
+      onChange={handleCountryNameChange}
+      value={countryName}
+      className="border py-2 px-4 text-4xl rounded-4xl mt-8" />
       <div className="flex flex-wrap gap-4 mt-12 justify-center">
-        {countries.map(country => <CountryCard key={country.name.official} country={country} />)}
+        {countriesFiltered.map(country => <CountryCard key={country.name.official} country={country} />)}
       </div>
     </div>
   )
