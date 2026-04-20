5d81c338c4d78e620a93beaf4c81cc812bd1cdd0
CCode02 <cristiancameloariza@gmail.com>
2026-04-20 15:25:23 -0500

    Nombre del pais añadido al tooltip y cambio del cursor

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,6 +1,8 @@
 const CountryCard = ({ country }) => {
-    return <div className="size-24 p-2 flex flex-col justify-center items-center border rounded-2xl shadow-sm/20
-    hover:scale-110 hover:shadow-md/20 transition-all">
+    return <div
+        className="size-24 p-2 flex flex-col justify-center items-center border rounded-2xl shadow-sm/20 cursor-pointer
+    hover:scale-110 hover:shadow-md/20 transition-all"
+        title={country.name.common} >
         <img src={country.flags.svg} alt={country.flags.alt} className="h-1/2 w-auto" />
     </div>
 }
