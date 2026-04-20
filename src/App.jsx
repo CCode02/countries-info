d35197bfb884a8db1548a2f8eec345c505b06b50
CCode02 <cristiancameloariza@gmail.com>
2026-04-20 12:33:51 -0500

    Creación de el titulo y el input de la pagina inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 function App() {
 
   return (
-    <>
-    </>
+    <div className="flex flex-col w-full h-screen p-12 items-center box-border">
+    <h1 className="text-6xl font-bold">
+      Search Country
+      </h1>
+    <input type="text" className="border py-2 px-4 text-4xl rounded-4xl mt-8" />
+    </div>
   )
 }
 
